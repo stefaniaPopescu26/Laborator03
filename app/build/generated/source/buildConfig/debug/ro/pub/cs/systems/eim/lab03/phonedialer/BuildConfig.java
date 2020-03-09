3d3c649ccfc6f45273f7fc480dbd065d611f79c9
Student USO VM User <student@stud.acs.upb.ro>
2020-03-09 14:00:12 +0200

    implemented taks for laboratory 03

---
+++ app/build/generated/source/buildConfig/debug/ro/pub/cs/systems/eim/lab03/phonedialer/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package ro.pub.cs.systems.eim.lab03.phonedialer;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "ro.pub.cs.systems.eim.lab03.phonedialer";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
---
+++ app/src/main/java/ro/pub/cs/systems/eim/lab03/phonedialer/PhoneDialerActivity.java
@@ -0,0 +1,16 @@
+package ro.pub.cs.systems.eim.lab03.phonedialer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PhoneDialerActivity extends AppCompatActivity {
+
+    private class PhoneDialerButtonPressListener
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
